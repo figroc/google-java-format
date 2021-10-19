fd344d1fd123e9b071c96827b8fbc2337f02e69e
P Chen <figroc@gmail.com>
2021-10-19 14:02:30 +0800

    feat: gradle build

--- src/main/java/com/google/googlejavaformat/java/Main.java
+++ src/main/java/com/google/googlejavaformat/java/Main.java
@@ -42,7 +42,7 @@ public final class Main {
   private static final String STDIN_FILENAME = "<stdin>";
 
   static String versionString() {
-    return "google-java-format: Version " + GoogleJavaFormatVersion.version();
+    return "google-java-format: Version " + Main.class.getPackage().getImplementationVersion();
   }
 
   private final PrintWriter outWriter;
